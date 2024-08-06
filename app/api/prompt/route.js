a357f6874ea9091047b03ac3b622d60cfcd5d298
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-08-07 00:44:52 +0300

    Rolback

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,14 +7,7 @@ export const GET = async (request) => {
 
 		const prompts = await Prompt.find({}).populate('creator');
 
-		// Set Cache-Control headers
-		const headers = new Headers({
-			'Cache-Control': 'no-cache, no-store, must-revalidate', // Adjust as needed
-			Pragma: 'no-cache',
-			Expires: '0',
-		});
-
-		return new Response(JSON.stringify(prompts), { status: 200, headers });
+		return new Response(JSON.stringify(prompts), { status: 200 });
 	} catch (error) {
 		return new Response('Failed to fetch all prompts', { status: 500 });
 	}
